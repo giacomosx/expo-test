cf6606a2c5d13591804f52c85e3fa74170f18df4
giacomosx <giacomo.bartoli@me.com>
2025-04-23 17:31:03 +0200

    replace tab bar

---
+++ app/(tabs)/inbox.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+const Inbox = () => {
+    return (
+        <View className="flex-1 justify-center items-center bg-[#25292e]">
+            <Text className={'text-white'}>Inbox screen</Text>
+        </View>
+    );
+};
+
+export default Inbox;
---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+const Search = () => {
+    return (
+        <View className="flex-1 justify-center items-center bg-[#25292e]">
+            <Text className={'text-white'}>Search screen</Text>
+        </View>
+    );
+};
+
+export default Search;
